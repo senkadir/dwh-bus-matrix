d0393a4a9f032b291c40836220ec5632252489a8
Abdulkadir Şen <abdulkadir.sen.external@cpp.canon>
2021-11-22 00:07:16 +0100

    matrix table created

---
+++ src/Matrix.Domain/Models/Matrix/ViewMatrixModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Matrix.Domain.Models
+{
+    public class ViewMatrixModel
+    {
+        public List<ViewDimensionModel> Dimensions { get; set; }
+
+        public IEnumerable<ViewFactModel> Facts{ get; set; }
+    }
+}
---
+++ src/Matrix.UI/Controllers/MatrixController.cs
@@ -0,0 +1,32 @@
+﻿using Matrix.Core.Services;
+using Matrix.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Matrix.UI.Controllers
+{
+    [Route("matrix")]
+    public class MatrixController : BaseController
+    {
+        private readonly IFactService _factService;
+        private readonly IDimensionService _dimensionService;
+
+        public MatrixController(IFactService factService,
+                                IDimensionService dimensionService)
+        {
+            _factService = factService;
+            _dimensionService = dimensionService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewMatrixModel model = new()
+            {
+                Dimensions = await _dimensionService.GetAsync(),
+                Facts = await _factService.GetAsync()
+            };
+
+            return View(model);
+        }
+    }
+}
